1637f976ef63a7cadc20b3ef2c06f8e01bf53e23
Daniel <d.barriele@gmail.com>
2021-02-04 00:55:21 +0100

    Intentamos Whiles

--- Escape_tierra/entidades.py
+++ Escape_tierra/entidades.py
@@ -8,7 +8,7 @@ pg.init()
 
 #Inicio
 
-class Pulsa_Space:
+class Inicio:
 
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -16,14 +16,12 @@ class Pulsa_Space:
         self.vx = vx
         self.vy = vy
 
-        self.imagen = pg.image.load("recursos/imagenes/Pulsa_Space.jpg")
-        
-    def comezar_juego(self):
-#        if event.type == pg.KEYSPACE:
-#            if event.key == pg.K_SPACE:
-        pass
+        self.imagen = pg.image.load("recursos/imagenes/Inicio.jpg")
 
-class Pantalla_inicio:
+    def pantalla_inicio(self):
+        self.imagen = pg.image.load("recursos/imagenes/Inicio.jpg")
+
+class Pantalla_historia:
 
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -31,7 +29,7 @@ class Pantalla_inicio:
         self.vx = vx
         self.vy = vy
 
-        self.imagen = pg.image.load("recursos/imagenes/Inicio.png")
+        self.imagen = pg.image.load("recursos/imagenes/Historia.png")
 
 #Clases Juego
 
@@ -42,7 +40,7 @@ class Asteroide:
         self.vx = vx
         self.vy = vy
 
-        self.imagen = pg.image.load("recursos/imagenes/Asteroide.png")
+        self.imagen = pg.image.load("recursos/imagenes/Asteroide_A.png")
 
 class Nave:
     def __init__(self, x, y, vx, vy):
@@ -82,23 +80,28 @@ class Game_over:
 
         self.imagen = pg.image.load("recursos/imagenes/Gameover.png")
 
+    def game_over_final(self):
+        pg.image.load("recursos/imagenes/Gameover.png")
+
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode((1366, 768))
         pg.display.set_caption("Escape de la tierra")
 
-        self.pulsa_space = Pulsa_Space(0, 0, 0, 0)
-        self.inicio = Pantalla_inicio(0, 931, 0, 10)
+        self.inicio = Inicio(0, 0, 0, 0)
+        self.historia = Pantalla_historia(0, 931, 0, 50)
         self.fondo_estrellado = Fondo(0, 0, 0, 0)
         self.asteroide = Asteroide(1366, random.randint(0, 768), -3, 0)
         self.nave = Nave (0, 384, 0, 10)
+        self.marcador = pg.font.Font("recursos/fuente/OpenSans-Bold.ttf", 24)
+        self.acero = 0
         self.game_over = Game_over (0, 0, 0, 0)
         self.clock = pg.time.Clock()
 
     def bucle_pricipal(self):
-        game_over = False
+        cierre = False
 
-        while not game_over:
+        while not cierre:
             self.clock.tick(FPS)
 
 # Gestion de eventos
@@ -111,35 +114,30 @@ class Game:
 
                 if event.type == pg.KEYUP:
                     if event.key == pg.K_UP:
-                        self.nave.y -= 10
+                        self.nave.y -= 25
                         if self.nave.y <= 0:
                             self.nave.y = 0
 
                 if event.type == pg.KEYDOWN:
                     if event.key == pg.K_DOWN:
-                        self.nave.y += 10
+                        self.nave.y += 25
                         if self.nave.y + 72 >= 768:
                             self.nave.y = 768 - 72 
 
-#            teclas_pulsadas = pg.key.get_pressed()
-#            if teclas_pulsadas[K_UP]:
-#                self.nave.y -= 10
-#                if self.nave.y <= 0:
-#                    self.nave.y = 0
-
-#            if teclas_pulsadas[K_DOWN]:
-#                self.nave.y += 10
-#                if self.nave.y + 72 >= 768:
-#                    self.nave.y = 768 - 72    
+                if event.type == pg.K_SPACE:
+                    if event.key == pg.K_SPACE:
+                        self.has_pulsado = True
 
 # Insertamos los objetos
 
-#            self.pantalla.blit(self.pulsa_space.imagen, (self.pulsa_space.x, self.pulsa_space.y))
+#            self.pantalla.blit(self.inicio.imagen, (self.inicio.x, self.inicio.y))
 
-            self.pantalla.blit(self.inicio.imagen, (self.inicio.x, self.inicio.y))
-            self.inicio.y -= self.inicio.vy
+            self.pantalla.blit(self.historia.imagen, (self.historia.x, self.historia.y))
+            self.historia.y -= self.historia.vy
 
-            if self.inicio.y + 931 <= self.inicio.x:
+            self.inicio.pantalla_inicio()
+
+            if self.historia.y + 931 <= self.historia.x:
 
                 self.pantalla.blit(self.fondo_estrellado.imagen, (self.fondo_estrellado.x, self.fondo_estrellado.y))
 
@@ -148,21 +146,27 @@ class Game:
 
                 self.pantalla.blit(self.nave.imagen, (self.nave.x, self.nave.y))
 
-                if self.nave.comprobar_colision(self.asteroide):
-                    self.pantalla.blit(self.game_over.imagen, (self.game_over.x, self.game_over.y))
-
-
-#                if pasa mas de 50 Segudos
-#                    Animacion de llegada al planeta
-#                    Introduce tu nombre - Sqlite
+                puntuacion = self.marcador.render(str(self.acero), True, (255,255,255))
+                self.pantalla.blit(puntuacion, (10, 10))
 
 #Funcionalidades
 
             self.nave.comprobar_colision(self.asteroide)
 
-            if self.asteroide.x < 0:
-                self.asteroide = Asteroide(1366, random.randint(0, 768), -0.5, 0)
-            self.asteroide.vx = self.asteroide.vx -1
+            if self.nave.comprobar_colision(self.asteroide): 
+                self.pantalla.blit(self.game_over.imagen, (self.game_over.x, self.game_over.y))
+
+            if self.asteroide.x < -50:
+                self.asteroide = Asteroide(1366, random.randint(0, 768), 0, 0)
+            self.asteroide.vx = self.asteroide.vx -0.25
+            self.acero = self.acero +1
+
+            if self.acero >= 1000:
+                self.acero = 1000
+                self.nave.x += self.nave.vx +5
+            elif self.nave.x >= 1366:
+                self.pantalla.blit(self.game_over.imagen, (self.game_over.x, self.game_over.y))
+
 
 # Refrescamos
 
